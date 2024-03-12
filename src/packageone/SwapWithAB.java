0ddeb2c88bf53787536ce992f2fe6ad366c3dffa
Dhaneshwari <umaranidp2601@gmail.com>
2024-03-12 19:12:39 +0530

    assignment

---
+++ src/packageone/SwapWithAB.java
@@ -0,0 +1,15 @@
+package packageone;
+
+public class SwapWithAB 
+{
+public static void main(String[] args) 
+{
+ int a=15;
+ int b=12;
+ a=a+b;
+ b=a-b;
+ a=a-b;
+ System.out.println("Value of a is: "+a);
+System.out.println("Value of b is: "+b);
+}
+}
---
+++ src/packageone/SwapWithC.java
@@ -0,0 +1,19 @@
+package packageone;
+
+public class SwapWithC 
+{
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		int b=22;
+		int c=a;
+		 a=b;
+		 b=c;
+		System.out.println("Value of a is: "+a);
+		System.out.println("Value of b is: "+b);
+		//System.out.println(c);
+
+	}
+
+}
